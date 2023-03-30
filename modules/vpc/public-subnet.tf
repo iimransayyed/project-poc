e9fd7841748295fc9bae8e8882ba33061443bbff
iimransayyed <iimransayyed@outlook.com>
2023-03-30 12:09:19 +0530

    pub sub RTconfig

--- modules/vpc/public-subnet.tf
+++ modules/vpc/public-subnet.tf
@@ -15,3 +15,16 @@ resource "aws_subnet" "public_subnet_az_a" {
     var.additional_tags
   )
 }
+# Configuration section for route table public subnet
+resource "aws_route_table" "public_subnet" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = merge(
+    {
+      "Name" = format(
+        "${var.environment_name}-${var.public_subnet_interfix}",
+      )
+    },
+    var.additional_tags
+  )
+}
